ede8bf5a0b140c521727f69798a86cb77a17f0ff
dofragle <48375238+dofragle@users.noreply.github.com>
2020-12-08 22:52:23 +1000

    Update task2.c
    процесс

--- task2.c
+++ task2.c
@@ -11,19 +11,35 @@
 
 int main(int argc, char **argv)
 {
+    char str[1000];
     int f;
     f = open(argv[1], O_RDWR)
     if (f==-1) perror(argv[1]);
 
     int tmp_c;
     off_t pos = 0;
-
+    int len = 0;
+    int fl=0;
     while((read(f, &tmp_c, sizeof(char))) > 0)
     {
         if(c='\n')
         {
-            pos = lseek
+            pos = lseek(f, 0, SEEK_CUR);
+            fl=0;
         }
+        else
+        {
+            if(!fl)
+            {
+                for(int i=0; (len-1); i++)
+                {
+                    if (str[len] == str[i]) fl=1;                
+                }
+            }
+            str[len] = c;
+            len++
+        }
+        
     }
 
     return 0;
