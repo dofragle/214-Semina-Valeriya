ce4d39cff26de55ea6a02003aca9ab1a6977daed
dofragle <48375238+dofragle@users.noreply.github.com>
2020-12-08 22:03:11 +1000

    Update task1.c
    в процессе

--- task1.c
+++ task1.c
@@ -1,1 +1,64 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <limits.h>
+#include <fcntl.h> 
 
+
+
+int main(int argc, char **argv)
+{
+    int fd[2];
+    pipe(fd);
+    int f;
+    
+    if(fork() == 0)
+    {
+        f = open(argv[4], O_RDONLY);
+        if(f==-1) perror(argv[4]);
+        dup2 (f, 0);
+        dup2(fd[1], 1);
+        
+        close(f);
+        close(fd[0]);
+        close(fd[1]);
+
+        execlp(argv[3], argv[3], NULL);
+        exit(1);
+    }
+
+    if(fork() == 0)
+    {
+        dup2(fd[0], 0);
+        
+        close(fd[0]);
+        close(fd[1]);
+
+        if(!fork())execlp(argv[1], argv[1], NULL);
+
+        int status;
+        wait(&status);
+        if(WEXITSTATUS(status))
+        {
+            if (WEXITSTATUS(status)!=0)
+            {
+                if(!fork())
+                {
+                    execlp(argv[2], argv[2], NULL);
+                    exit(3);
+                }
+            } 
+        }
+        exit(2);
+    }
+ 
+    close(fd[0]);
+    close(fd[1]);
+    wait(0);
+    wait(0);
+    wait(0);
+    return 0;
+}
